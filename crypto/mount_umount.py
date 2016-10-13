b0ab889b5ff76007f66b7805ee6a969633ca513d
Bastien DHIVER <epitech@bastn.fr>
2016-10-13 10:42:40 +0200

    Improve libc dynamic loading for mount() and umount()

--- crypto/mount_umount.py
+++ crypto/mount_umount.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 import ctypes
+from ctypes.util import find_library
 import os
 
 def mount(source, target, fs):
     err_msg = ""
-    ret = ctypes.CDLL('libc.so.6', use_errno=True).mount(
+    ret = ctypes.CDLL(find_library('c'), use_errno=True).mount(
         ctypes.c_char_p(source.encode()), ctypes.c_char_p(target.encode()),
         ctypes.c_char_p(fs.encode()), 0, 0)
     if ret != 0:
@@ -17,7 +18,7 @@ def mount(source, target, fs):
 
 def umount(path):
     err_msg = ""
-    ret = ctypes.CDLL('libc.so.6', use_errno=True).umount(
+    ret = ctypes.CDLL(find_library('c'), use_errno=True).umount(
         ctypes.c_char_p(path.encode()))
     if ret != 0:
         errno = ctypes.get_errno()
