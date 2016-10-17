2fb689142101f6f8c6e358ebba64519a6336d997
Bastien DHIVER <epitech@bastn.fr>
2016-10-17 10:56:37 +0200

    Hardening isValid() for Parsing

--- luksypam/ParseConfig.py
+++ luksypam/ParseConfig.py
@@ -15,6 +15,8 @@ MANDATORY_ENTRIES = [('mount', bool),
                      ('weak', bool),
                      ('sizeInMB', int)]
 
+DEFAULT_MIN_CONTAINER_SIZE = 8
+
 class ParseConfig:
     def __init__(self, path):
         assert isinstance(path, str)
@@ -32,10 +34,19 @@ class ParseConfig:
         return True
 
     def isEmpty(self):
-        return False if len(self.data) != 0 else True
+        if self.data:
+            return False if len(self.data) != 0 else True
+        return True
 
     def isValid(self):
+        if not self.data:
+            return False
         for container in self.data:
+            if '../' in container or '/..' in container:
+                logger.log(logging.ERROR,
+                           "Error: container name '{}' path must not contains '..'"
+                           .format(container))
+                return False
             for key in MANDATORY_ENTRIES:
                 if not key[0] in self.data[container]:
                     logger.log(logging.ERROR,
@@ -47,6 +58,23 @@ class ParseConfig:
                                "Error in {} config, '{}' must be of type {}"
                                .format(container, key[0], key[1].__name__))
                     return False
+                if key[0] == 'mountDir':
+                    if not self.data[container][key[0]]:
+                        logger.log(logging.ERROR,
+                                   "Error in {} config, '{}' is empty"
+                                   .format(container, key[0]))
+                        return False
+                    if '../' in self.data[container][key[0]] or '/..' in self.data[container][key[0]]:
+                        logger.log(logging.ERROR,
+                                   "Error in {} config, '{}' path must not contains '..'"
+                                   .format(container, key[0]))
+                        return False
+                if key[0] == 'sizeInMB':
+                    if self.data[container][key[0]] < DEFAULT_MIN_CONTAINER_SIZE:
+                        logger.log(logging.ERROR,
+                                   "Error in {} config, '{}' size must but at least {}"
+                                   .format(container, key[0], DEFAULT_MIN_CONTAINER_SIZE))
+                        return False
         return True
 
     def getContent(self):
