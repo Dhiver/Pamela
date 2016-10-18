2b007e0cf87f01906280c9c89f98523b8f9866fb
Bastien DHIVER <epitech@bastn.fr>
2016-10-18 15:36:11 +0200

    Remove format.py

--- luksypam/format.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-def formatDeviceInExt4(path):
-    p = subprocess.Popen("mkfs.ext4 {}".format(path), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    out, err = p.communicate()
-    return p.returncode, err
