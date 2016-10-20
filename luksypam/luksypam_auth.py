b1e8ef3648c240e89add193c0166d83539f5faaa
Bastien DHIVER <epitech@bastn.fr>
2016-10-20 11:37:45 +0200

    Rename luksypam.py

--- luksypam/luksypam_auth.py
+++ luksypam/luksypam_auth.py
@@ -2,19 +2,11 @@
 
 import os
 import sys
-import psutil
 import logging
 from luksypam_log import logger
 import LuksyPam
 
 password = input()[:-1]
-
-#users = list()
-#for user in psutil.users():
-#    users.append(user.name)
-#logger.log(logging.DEBUG, "Logged users : {}".format(users))
-
-logger.log(logging.DEBUG, "Env : {}".format(os.environ))
 
 MANDATORY_ENV_VARS = ['PAM_USER', 'PAM_TYPE']
 
@@ -29,10 +21,19 @@ for key in MANDATORY_ENV_VARS:
 username = os.environ["PAM_USER"]
 action = os.environ["PAM_TYPE"]
 
-inst = LuksyPam.LuksyPam(username, password)
-
-if not inst.init() or not inst.isLuksypamEnabled() or not inst.loadConfs() or not inst.initContainers() or not inst.openContainers() or not inst.mountContainers():
+if action != "auth":
     sys.exit(0)
 
-logger.log(logging.INFO, "OK")
+inst = LuksyPam.LuksyPam(username, password)
+
+if not inst.init() or not inst.isLuksypamEnabled() or not inst.loadConfs():
+    sys.exit(0)
+
+if not inst.createContainers() or not inst.initContainers():
+    sys.exit(0)
+
+if not inst.openContainers() or not inst.mountContainers():
+    sys.exit(0)
+
+logger.log(logging.INFO, "auth OK")
 sys.exit(0)
